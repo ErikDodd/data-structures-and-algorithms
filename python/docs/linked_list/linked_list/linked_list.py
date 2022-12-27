8b23c0ad1cf74d7b47f147a16f3db2ab85d910ca
ERIK M. DODD <emdodd27@gmail.com>
2022-12-27 07:48:43 -0800

    Completed most of the requirements. Still need to return collection of all the values in the linked list

--- python/docs/linked_list/linked_list/linked_list.py
+++ python/docs/linked_list/linked_list/linked_list.py
@@ -1,46 +1,61 @@
-class Linked_List:
-    def __init__(self):
-        self.head = None
-
-
-    def insert(self, value):
-
-        pass
-
-
-    def includes(self):
-        pass
-
-
-    def __str__(self):
-        pass
-
-
 class Node:
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
 
+class Linked_List:
+    def __init__(self):
+        self.head = None
 
-def insert_linked_list():
+    linked_list = Linked_List()
+    linked_list_head = Node(7)
+    print(linked_list_head.value)
+
+    def insert(self, value=None):
+        newNode = Node(value)
+        if(self.head):
+            current = self.head
+            while(current.next):
+                current = current.next
+            current.next = newNode
+        else:
+            self.head = newNode
+
+
+    def includes(self, value):
+        current = self.head
+        while current != None:
+            if current.data == value:
+                return True
+            current = current.next
+        return False
+
+
+
+    def __str__(self, value):
+        return f"{ a } -> { b } -> { c } -> NULL"
+
+
+
+def insert_linked_list(self):
     pass
 
 
-def head():
+def head(self):
     pass
 
 
-def insert_multiple_nodes():
+def insert_multiple_nodes(self):
     pass
 
 
-def value_in_linked_list():
+def value_in_linked_list(self):
     pass
 
 
-def value_not_in_linked_list():
+def value_not_in_linked_list(self):
     pass
 
 
-def collection_values_in_linked_list():
+def collection_values_in_linked_list(self):
     pass
