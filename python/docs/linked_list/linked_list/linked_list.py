8a56f8bd17d52fea904f210d8d38865dc7273931
ERIK M. DODD <emdodd27@gmail.com>
2022-12-27 00:20:00 -0800

    Set-up completed. Working on LinkedList.py and Tests

---
+++ python/docs/linked_list/linked_list/linked_list.py
@@ -0,0 +1,46 @@
+class Linked_List:
+    def __init__(self):
+        self.head = None
+
+
+    def insert(self, value):
+
+        pass
+
+
+    def includes(self):
+        pass
+
+
+    def __str__(self):
+        pass
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+def insert_linked_list():
+    pass
+
+
+def head():
+    pass
+
+
+def insert_multiple_nodes():
+    pass
+
+
+def value_in_linked_list():
+    pass
+
+
+def value_not_in_linked_list():
+    pass
+
+
+def collection_values_in_linked_list():
+    pass
