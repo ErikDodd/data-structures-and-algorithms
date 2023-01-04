fd57afddc743dd15ae6468e3845bcb738523d257
ERIK M. DODD <emdodd27@gmail.com>
2023-01-04 08:46:18 -0800

    Working on CC 12

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,10 +1,55 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+
+    def enqueue(self, animal):
+        if self.rear is None:
+            temp = Node(animal)
+            self.rear.push(temp)
+            return self
+        if self.rear:
+            temp = Node(animal)
+            self.rear
+
+
+        pass
+        #if self.rear:
+            #self.rear.next = Node(animal)
+            #self.rear = self.rear.next
+            #return
+        #self.rear = self.front = Node(animal)
+
+
+    def dequeue(self, perf):
+
+        pass
+        #if self.front is None:
+            #raise InvalidOperationError
+        #dequeued = self.front
+        #self.front = self.front.next
+
+    class InvalidOperationError(Exception):
+        def __int__(self):
+            pass
+        def __str__(self):
+            return self
 
 
 class Dog:
-    pass
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 
 
 class Cat:
-    pass
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
