4e4e91425c7ce0939ce07aad404de8a47f0b5219
Erik M. Dodd <emdodd27@gmail.com>
2023-01-06 17:02:50 -0800

    1 Test passing, 1 test failed, 1 test skipped

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -5,12 +5,37 @@ class BinaryTree:
 
     def __init__(self):
         # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
+    def post_order(self, root=None, nodes=None):
         # method body here
-        pass
+        if root is None:
+            root = self.root
+        if nodes is None:
+            nodes = []
+        if root.left:
+            self.post_order(root.left, nodes)
+        if root.right:
+            self.post_order(root.right, nodes)
+        nodes.append(root.value)
+        return nodes
+
+    def pre_order(self, root=None, nodes=None):
+        if root is None:
+            root = self.root
+            print(root)
+        if nodes in None:
+            nodes = []
+        if root.left:
+            self.pre_order(root.left)
+        if root.right:
+            self.pre_order(root.right)
+
+
 
 
 class Node:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
