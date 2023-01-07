32e6f5f7b4a4aba53d159969b58f267c0f6651bc
Erik M. Dodd <emdodd27@gmail.com>
2023-01-07 12:51:09 -0800

    Working on Binary_Search_tree 4 passed, 4 skipped, 1 failed

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -8,8 +8,27 @@ class BinarySearchTree(BinaryTree):
 
     def __init__(self):
         # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
+    # Adds a new node with that value in the correct location in the binary search tree.
+    def add(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return self.root.value
+        while True:
+            if self.root.left is None:
+                self.root.left = new_node
+                return self.root.left.value
+            if self.root.right is None:
+                self.root.right = new_node
+                return self.root.right.value
+
+
+
+
+
+
+    def contains(self):
+        #Returns: boolean indicating whether or not the value is in the tree at least once.
         pass
