4f593b93560f707a2613db320684b1c8a7780713
Erik M. Dodd <emdodd27@gmail.com>
2023-01-09 23:07:38 -0800

    All tests passing for CC13. Need to add ReadME and finish whiteboard

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,2 +1,15 @@
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    brackets = {'(': ')', '[': ']', '{': '}'}
+    stack = []
+    for char in string:
+        if char in brackets:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            elif brackets[stack[-1]] != char:
+                return False
+            else:
+                stack.pop()
+    return not stack
+
